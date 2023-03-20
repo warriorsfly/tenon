448e402610af9a56586f2c7b25467fd499b0d607
walker <warriorsfly@gmail.com>
2023-03-20 23:05:09 +0800

    fix user error

--- app/models/user.py
+++ app/models/user.py
@@ -9,7 +9,7 @@ from .. import db
 class User(UserMixin, db.Model):
     __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(64), unique=True, index=True))
+    email = db.Column(db.String(64), unique=True, index=True)
     username = db.Column(db.String(64), unique=True, index=True)
     password_hash = db.Column(db.String(128))
     confirmed = db.Column(db.Boolean, default=False)
